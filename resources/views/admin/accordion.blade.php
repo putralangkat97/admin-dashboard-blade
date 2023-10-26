72681c6362e9c6fa1e448c8ad706a72c9099827f
Anggit Utomo <pucukpisang97@gmail.com>
2023-10-26 23:12:05 +0700

    refactoring accordion

--- resources/views/admin/accordion.blade.php
+++ resources/views/admin/accordion.blade.php
@@ -5,189 +5,63 @@
         </h2>
     </x-slot>
 
-    <h1 class="mb-3">Basic Accordion</h1>
-    <div class="p-6 border mb-8" x-data="{ showCode: false }">
-        <div class="flex mb-3">
-            <div class="flex bg-gray-100 transition p-2">
-                <nav class="flex space-x-2">
-                    <button type="button" class="py-3 px-4 inline-flex items-center gap-2 text-sm font-medium"
-                        :class="showCode === false ? 'bg-blue-500 text-white hover:text-white' :
-                            'text-gray-500 hover:text-gray-600'"
-                        @click="showCode = false" role="tab">
-                        Preview
-                    </button>
-                    <button type="button" class="py-3 px-4 inline-flex items-center gap-2 text-sm font-medium"
-                        :class="showCode === true ? 'bg-blue-500 text-white hover:text-white' :
-                            'text-gray-500 hover:text-gray-600'"
-                        @click="showCode = true" role="tab">
-                        Code
-                    </button>
-                </nav>
-            </div>
-        </div>
-        {{-- preview --}}
+    <x-content-base title="Basic Usage" eventName="showCode">
+        <x-tabs-preview-code />
         <div x-show="!showCode">
             <x-accordions.accordion-default />
         </div>
-
-        {{-- code --}}
         <div x-show="showCode">
             <x-torchlight.accordion type="default" />
         </div>
-    </div>
+    </x-content-base>
 
-    <h1 class="mb-3">Icon Accordion</h1>
-    <div class="p-6 border mb-8" x-data="{ showCode2: false }">
-        <div class="flex mb-3">
-            <div class="flex bg-gray-100 transition p-2">
-                <nav class="flex space-x-2">
-                    <button type="button" class="py-3 px-4 inline-flex items-center gap-2 text-sm font-medium"
-                        :class="showCode2 === false ? 'bg-blue-500 text-white hover:text-white' :
-                            'text-gray-500 hover:text-gray-600'"
-                        @click="showCode2 = false" role="tab">
-                        Preview
-                    </button>
-                    <button type="button" class="py-3 px-4 inline-flex items-center gap-2 text-sm font-medium"
-                        :class="showCode2 === true ? 'bg-blue-500 text-white hover:text-white' :
-                            'text-gray-500 hover:text-gray-600'"
-                        @click="showCode2 = true" role="tab">
-                        Code
-                    </button>
-                </nav>
-            </div>
-        </div>
-        {{-- preview --}}
+    <x-content-base title="With Icon" eventName="showCode2">
+        <x-tabs-preview-code eventName="showCode2" />
         <div x-show="!showCode2">
             <x-accordions.accordion-default icon="true" />
         </div>
-
-        {{-- code --}}
         <div x-show="showCode2">
             <x-torchlight.accordion type="icon" />
         </div>
-    </div>
+    </x-content-base>
 
-    <h1 class="mb-3">Basic Accordion with Colour</h1>
-    <div class="p-6 border mb-8" x-data="{ showCode3: false }">
-        <div class="flex mb-3">
-            <div class="flex bg-gray-100 transition p-2">
-                <nav class="flex space-x-2">
-                    <button type="button" class="py-3 px-4 inline-flex items-center gap-2 text-sm font-medium"
-                        :class="showCode3 === false ? 'bg-blue-500 text-white hover:text-white' :
-                            'text-gray-500 hover:text-gray-600'"
-                        @click="showCode3 = false" role="tab">
-                        Preview
-                    </button>
-                    <button type="button" class="py-3 px-4 inline-flex items-center gap-2 text-sm font-medium"
-                        :class="showCode3 === true ? 'bg-blue-500 text-white hover:text-white' :
-                            'text-gray-500 hover:text-gray-600'"
-                        @click="showCode3 = true" role="tab">
-                        Code
-                    </button>
-                </nav>
-            </div>
-        </div>
-        {{-- preview --}}
+    <x-content-base title="With Colour" eventName="showCode3">
+        <x-tabs-preview-code eventName="showCode3" />
         <div x-show="!showCode3">
             <x-accordions.accordion-default colour="gray" icon="true" />
         </div>
-
-        {{-- code --}}
         <div x-show="showCode3">
             <x-torchlight.accordion type="colour" />
         </div>
-    </div>
+    </x-content-base>
 
-    <h1 class="mb-3">Collapse Accordion</h1>
-    <div class="p-6 border mb-8" x-data="{ showCode4: false }">
-        <div class="flex mb-3">
-            <div class="flex bg-gray-100 transition p-2">
-                <nav class="flex space-x-2">
-                    <button type="button" class="py-3 px-4 inline-flex items-center gap-2 text-sm font-medium"
-                        :class="showCode4 === false ? 'bg-blue-500 text-white hover:text-white' :
-                            'text-gray-500 hover:text-gray-600'"
-                        @click="showCode4 = false" role="tab">
-                        Preview
-                    </button>
-                    <button type="button" class="py-3 px-4 inline-flex items-center gap-2 text-sm font-medium"
-                        :class="showCode4 === true ? 'bg-blue-500 text-white hover:text-white' :
-                            'text-gray-500 hover:text-gray-600'"
-                        @click="showCode4 = true" role="tab">
-                        Code
-                    </button>
-                </nav>
-            </div>
-        </div>
-        {{-- preview --}}
+    <x-content-base title="Collapse Accordion" eventName="showCode4">
+        <x-tabs-preview-code eventName="showCode4" />
         <div x-show="!showCode4">
             <x-accordions.accordion-collapse />
         </div>
-
-        {{-- code --}}
         <div x-show="showCode4">
             <x-torchlight.accordion type="collapse" />
         </div>
-    </div>
+    </x-content-base>
 
-    <h1 class="mb-3">Collapse Accordion with Icon</h1>
-    <div class="p-6 border mb-8" x-data="{ showCode5: false }">
-        <div class="flex mb-3">
-            <div class="flex bg-gray-100 transition p-2">
-                <nav class="flex space-x-2">
-                    <button type="button" class="py-3 px-4 inline-flex items-center gap-2 text-sm font-medium"
-                        :class="showCode5 === false ? 'bg-blue-500 text-white hover:text-white' :
-                            'text-gray-500 hover:text-gray-600'"
-                        @click="showCode5 = false" role="tab">
-                        Preview
-                    </button>
-                    <button type="button" class="py-3 px-4 inline-flex items-center gap-2 text-sm font-medium"
-                        :class="showCode5 === true ? 'bg-blue-500 text-white hover:text-white' :
-                            'text-gray-500 hover:text-gray-600'"
-                        @click="showCode5 = true" role="tab">
-                        Code
-                    </button>
-                </nav>
-            </div>
-        </div>
-        {{-- preview --}}
+    <x-content-base title="Collapse with Icon" eventName="showCode5">
+        <x-tabs-preview-code eventName="showCode5" />
         <div x-show="!showCode5">
             <x-accordions.accordion-collapse icon="true" />
         </div>
-
-        {{-- code --}}
         <div x-show="showCode5">
             <x-torchlight.accordion type="icon-collapse" />
         </div>
-    </div>
+    </x-content-base>
 
-    <h1 class="mb-3">Collapse Accordion with Colour</h1>
-    <div class="p-6 border mb-8" x-data="{ showCode6: false }">
-        <div class="flex mb-3">
-            <div class="flex bg-gray-100 transition p-2">
-                <nav class="flex space-x-2">
-                    <button type="button" class="py-3 px-4 inline-flex items-center gap-2 text-sm font-medium"
-                        :class="showCode6 === false ? 'bg-blue-500 text-white hover:text-white' :
-                            'text-gray-500 hover:text-gray-600'"
-                        @click="showCode6 = false" role="tab">
-                        Preview
-                    </button>
-                    <button type="button" class="py-3 px-4 inline-flex items-center gap-2 text-sm font-medium"
-                        :class="showCode6 === true ? 'bg-blue-500 text-white hover:text-white' :
-                            'text-gray-500 hover:text-gray-600'"
-                        @click="showCode6 = true" role="tab">
-                        Code
-                    </button>
-                </nav>
-            </div>
-        </div>
-        {{-- preview --}}
+    <x-content-base title="Collapse with Colour" eventName="showCode6">
+        <x-tabs-preview-code eventName="showCode6" />
         <div x-show="!showCode6">
             <x-accordions.accordion-collapse colour="gray" icon="true" />
         </div>
-
-        {{-- code --}}
         <div x-show="showCode6">
             <x-torchlight.accordion type="colour-collapse" />
         </div>
-    </div>
+    </x-content-base>
 </x-app-layout>
---
+++ resources/views/components/tabs-preview-code.blade.php
@@ -0,0 +1,22 @@
+@props([
+    'eventName' => 'showCode'
+])
+
+<div class="flex mb-3">
+    <div class="flex bg-gray-100 transition p-2">
+        <nav class="flex space-x-2">
+            <button type="button" class="py-3 px-4 inline-flex items-center gap-2 text-sm font-medium"
+                :class="{{ $eventName }} === false ? 'bg-blue-500 text-white hover:text-white' :
+                    'text-gray-500 hover:text-gray-600'"
+                @click="{{ $eventName }} = false">
+                Preview
+            </button>
+            <button type="button" class="py-3 px-4 inline-flex items-center gap-2 text-sm font-medium"
+                :class="{{ $eventName }} === true ? 'bg-blue-500 text-white hover:text-white' :
+                    'text-gray-500 hover:text-gray-600'"
+                @click="{{ $eventName }} = true">
+                Code
+            </button>
+        </nav>
+    </div>
+</div>
